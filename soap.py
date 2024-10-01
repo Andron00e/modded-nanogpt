b27aba69a7dbb5ae82decebe993522e2637140d3
KellerJordan <kellerjordan1998@gmail.com>
2024-10-01 18:42:00 +0000

    .

--- soap.py
+++ soap.py
@@ -7,7 +7,7 @@ class SOAP(torch.optim.Optimizer):
     def __init__(self, params, lr=3e-3, betas=(0.95, 0.95), shampoo_beta=-1, eps=1e-8, precondition_frequency=10):
         defaults = dict(lr=lr, betas=betas, shampoo_beta=shampoo_beta, eps=eps, precondition_frequency=precondition_frequency)
         super().__init__(params, defaults)
-        
+
     def step(self):
         """
         Performs a single optimization step.
@@ -21,13 +21,10 @@ class SOAP(torch.optim.Optimizer):
                 grad = p.grad
 
                 state = self.state[p]
-
                 state['step'] = state.get('step', 0) + 1
                 if state['step'] == 1:
-                    # Exponential moving average of gradient values
-                    state["exp_avg"] = torch.zeros_like(grad)
-                    # Exponential moving average of squared gradient values
-                    state["exp_avg_sq"] = torch.zeros_like(grad)
+                    state["exp_avg"] = torch.zeros_like(grad) # Exponential moving average of gradient values
+                    state["exp_avg_sq"] = torch.zeros_like(grad) # Exponential moving average of squared gradient values
                     state['GG'] = [torch.zeros(d, d, device=grad.device) for d in grad.shape] # Will hold all the preconditioner matrices (L and R in the paper).
                     state['precondition_frequency'] = group['precondition_frequency']
                     state['shampoo_beta'] = group['shampoo_beta'] if group['shampoo_beta'] >= 0 else group["betas"][1]
@@ -40,7 +37,6 @@ class SOAP(torch.optim.Optimizer):
                 grad_projected = self.project(grad, state)
 
                 exp_avg, exp_avg_sq = state["exp_avg"], state["exp_avg_sq"]
-
                 # Decay the first and second moment running average coefficient
                 # In-place operations to update the averages at the same time
                 exp_avg.lerp_(grad, 1-beta1)
@@ -49,18 +45,17 @@ class SOAP(torch.optim.Optimizer):
                 # Projecting the exponential moving average of gradients to the eigenbases of Shampoo's preconditioner 
                 # i.e. projecting to the eigenbases of matrices in state['GG']
                 exp_avg_projected = self.project(exp_avg, state)
-                
-                step_size = group["lr"]
-                bias_correction1 = 1.0 - beta1 ** (state["step"])
-                bias_correction2 = 1.0 - beta2 ** (state["step"])
-                step_size = step_size * (bias_correction2 ** .5) / bias_correction1
 
                 # Projecting back the preconditioned (by Adam) exponential moving average of gradients
                 # to the original space
                 denom = exp_avg_sq.sqrt().add_(group["eps"])
                 norm_grad = self.project_back(exp_avg_projected / denom, state)
 
-                p.data.add_(norm_grad, alpha=-step_size)
+                step_size = group["lr"]
+                bias_correction1 = 1 - beta1**state["step"]
+                bias_correction2 = 1 - beta2**state["step"]
+                step_correction = (bias_correction2 ** .5) / bias_correction1
+                p.data.add_(norm_grad, alpha=-step_size * step_correction)
 
                 # Update is done after the gradient step to avoid using current gradients in the projection.
                 self.update_preconditioner(grad, state)
