733631af289b583709001cdee0ef84779d9a3a27
KellerJordan <kellerjordan1998@gmail.com>
2024-10-01 15:46:06 +0000

    .

--- train_gpt2.py
+++ train_gpt2.py
@@ -367,7 +367,7 @@ if __name__ == "__main__":
     # init the model from scratch
     num_vocab = 50257
     model_config = {
-        "d12": GPTConfig(vocab_size=num_vocab, n_layer=12, n_head=12, n_embd=768),
+        "d12": GPTConfig(vocab_size=num_vocab, n_layer=4, n_head=6, n_embd=384),
         "d24": GPTConfig(vocab_size=num_vocab, n_layer=24, n_head=16, n_embd=1024),
         "d36": GPTConfig(vocab_size=num_vocab, n_layer=36, n_head=20, n_embd=1280),
         "d48": GPTConfig(vocab_size=num_vocab, n_layer=48, n_head=25, n_embd=1600),
