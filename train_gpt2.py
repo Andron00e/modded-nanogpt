fe9ef04760d62d3f4b38d24338b6974de956c110
KellerJordan <kellerjordan1998@gmail.com>
2024-10-05 15:45:18 +0000

    .

--- train_gpt2.py
+++ train_gpt2.py
@@ -98,16 +98,18 @@ class Adafactor(torch.optim.Optimizer):
         state['steps'] = state.get('steps', 0) + 1
         if 'momentum_buffer' not in state:
             state['momentum_buffer'] = torch.zeros_like(g)
-            state['momentum_buffer2'] = torch.zeros_like(g)
+            state['momentum_buffer2'] = torch.zeros_like(g[0])
+            state['momentum_buffer3'] = torch.zeros_like(g[:, 0])
         buf = state['momentum_buffer']
         buf2 = state['momentum_buffer2']
+        buf3 = state['momentum_buffer3']
         buf.lerp_(g, 1-momentum)
-        buf2.lerp_(g.square(), 0.01)
+        beta2 = 0.99
+        buf2.lerp_(g.square().mean(1), 1-beta2)
+        buf3.lerp_(g.square().mean(0), 1-beta2)
         numer = buf / (1 - momentum**state['steps'])
-        denom = buf2 / (1 - 0.99**state['steps'])
-
+        denom = torch.outer(buf2, buf3) / (1 - beta2**state['steps'])
         eps = 1e-7
-        denom = (denom.log().mean(0, keepdim=True) + denom.log().mean(1, keepdim=True) - denom.log().mean()).exp()
         update = numer / (eps + denom.sqrt())
         p.data.add_(update, alpha=-lr)
 
