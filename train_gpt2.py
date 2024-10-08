8c86484e0d43d9f10ab736d3f3775981ac97ddab
KellerJordan <kellerjordan1998@gmail.com>
2024-10-08 20:16:28 +0000

    .

--- train_gpt2.py
+++ train_gpt2.py
@@ -170,10 +170,10 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    vocab_size = 50257
-    n_layer = 12
-    n_head = 12
-    n_embd = 768
+    vocab_size : int = 50257
+    n_layer : int = 12
+    n_head : int = 12
+    n_embd : int = 768
 
 class GPT(nn.Module):
 
@@ -297,21 +297,21 @@ class DistributedDataLoader:
 @dataclass
 class Hyperparameters:
     # data hyperparams
-    input_bin = 'data/fineweb10B/fineweb_train_*.bin' # input .bin to train on
-    input_val_bin = 'data/fineweb10B/fineweb_val_*.bin' # input .bin to eval validation loss on
+    input_bin : str = 'data/fineweb10B/fineweb_train_*.bin' # input .bin to train on
+    input_val_bin : str = 'data/fineweb10B/fineweb_val_*.bin' # input .bin to eval validation loss on
     # optimization hyperparams
-    batch_size = 8*64 # batch size, in sequences, across all devices
-    device_batch_size = 64 # batch size, in sequences, per device
-    sequence_length = 1024 # sequence length, in tokens
-    num_iterations = 6676 # number of iterations to run
-    learning_rate = 0.0036
-    warmup_iters = 0
-    warmdown_iters = 2000 # number of iterations of linear warmup/warmdown for triangular or trapezoidal schedule
-    weight_decay = 0
+    batch_size : int = 8*64 # batch size, in sequences, across all devices
+    device_batch_size : int = 64 # batch size, in sequences, per device
+    sequence_length : int = 1024 # sequence length, in tokens
+    num_iterations : int = 6676 # number of iterations to run
+    learning_rate : float = 0.0036
+    warmup_iters : int = 0
+    warmdown_iters : int = 2000 # number of iterations of linear warmup/warmdown for triangular or trapezoidal schedule
+    weight_decay : float = 0
     # evaluation and logging hyperparams
-    val_loss_every = 128 # every how many steps to evaluate val loss? 0 for only at the end
-    val_tokens = 10485760 # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
-    save_every = 0 # every how many steps to save the checkpoint? 0 for only at the end
+    val_loss_every : int = 128 # every how many steps to evaluate val loss? 0 for only at the end
+    val_tokens : int = 10485760 # how many tokens of validation data? it's important to keep this fixed for consistent comparisons
+    save_every : int = 0 # every how many steps to save the checkpoint? 0 for only at the end
 args = Hyperparameters()
 
 # set up DDP (distributed data parallel). torchrun sets this env variable
