fe93868b3de7d44a442520b23c923122b500e480
KellerJordan <kellerjordan1998@gmail.com>
2024-10-09 12:45:52 +0000

    .

--- train_gpt2.py
+++ train_gpt2.py
@@ -311,7 +311,7 @@ class Hyperparameters:
     batch_size : int = 8*64 # batch size, in sequences, across all devices
     device_batch_size : int = 64 # batch size, in sequences, per device
     sequence_length : int = 1024 # sequence length, in tokens
-    num_iterations : int = 6676 # number of iterations to run
+    num_iterations : int = 6390 # number of iterations to run
     learning_rate : float = 0.0036
     warmup_iters : int = 0
     warmdown_iters : int = 2000 # number of iterations of linear warmup/warmdown for triangular or trapezoidal schedule
